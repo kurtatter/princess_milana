fc659b418153561f9f8f77d83a7bbf46e750e198
Stanislav Dzasokhov <kurtat_programmer@mail.ru>
2024-01-18 19:31:41 +0300

    Add aiogram python-dotenv

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    ...
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+
+import config
 
 
-if '__name__' == '__main__':
-    main()
+async def main():
+    print(config.BOT_TOKEN)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
