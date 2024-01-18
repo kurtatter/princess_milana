04e71276eb63585010c94187922b8651cc86ddec
Stanislav Dzasokhov <kurtat_programmer@mail.ru>
2024-01-18 19:54:56 +0300

    Bot start state

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import asyncio
 import logging
 import sys
-from os import getenv
 
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.utils.markdown import hbold
+from aiogram.enums import ParseMode
 
 import config
 
+dp = Dispatcher()
 
-async def main():
-    print(config.BOT_TOKEN)
+
+@dp.message(CommandStart())
+async def command_start_handler(message: types.Message) -> None:
+    await message.answer(f"Aloha! Welcome, {hbold(message.from_user.full_name)}!")
+
+
+async def main() -> None:
+    bot = Bot(config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
